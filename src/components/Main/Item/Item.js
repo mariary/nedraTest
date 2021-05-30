4fdf05a48fff06cb299decfe90869c23f20e12b0
mariarybakonenko <65123160+mariary@users.noreply.github.com>
2021-05-30 03:02:20 +0300

    new img

--- src/components/Main/Item/Item.js
+++ src/components/Main/Item/Item.js
@@ -5,7 +5,7 @@ export const Item = (props) => {
     const logo = {
         'DeLorean': 'https://img.pngio.com/delorean-dmc-logo-bedeutung-zeichen-logo-png-delorean-motor-company-png-3840_2160.png',
         'Aston Martin': 'https://upload.wikimedia.org/wikipedia/en/b/bd/Aston_Martin_Lagonda_brand_logo.png',
-        'Mustang': 'https://lh3.googleusercontent.com/proxy/grqDVKE1vZisjtq3pNtbQxtilC620mmeEjonYWoQLnxjML2AmptYhRnhwj6-17Ga3Fnf_4fRhktYd79lTx5KU15bm3jYtTo',
+        'Mustang': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO--K6rPgtw0Go9rUEg8zQ3H7_QGXjXfi5K8ZvDvu2PbOeHYvxWEe9XE5YicxTrg_jk1c&usqp=CAU',
         'Ford': 'https://logos-world.net/wp-content/uploads/2021/03/Ford-Logo-1917-1927.png',
         'Bentley Motors': 'https://purepng.com/public/uploads/large/purepng.com-bentley-motors-logobentleybentley-motorsbritishmanufacturerluxury-carsloho-1701527414234rxmpt.png',
         "Dodge Challenger":"https://www.schwinds-classicpartsstore.de/images/product_images/original_images/1970_Dodge_ChallengerArmaEmblem_b.jpg"
